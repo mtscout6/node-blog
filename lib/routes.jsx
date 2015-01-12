7d0c47adab2575739f6118ca01b6002189587c83
Matthew Smith <mtscout6@gmail.com>
2015-01-12 07:21:50 -0700

    Fix bug in active navitem selection

--- lib/routes.jsx
+++ lib/routes.jsx
@@ -3,14 +3,14 @@ var { DefaultRoute, Route, NotFoundRoute } = require('react-router');
 
 module.exports = () => {
   return [
-    <Route name="posts" path="/" handler={require('./components/Shell')}>
-      <DefaultRoute handler={require('./components/Home')} />
+    <Route path='/' handler={require('./components/Shell')}>
+      <Route name='posts' path='/' handler={require('./components/Home')} />
       <Route name='other' handler={require('./components/Other')} />
       <Route path='blog' handler={require('./components/NoOpHandler')}>
         <Route name='blog-post' handler={require('../posts/test')} />
         <Route name='blog-post2' handler={require('../posts/test2')} />
       </Route>
-      <NotFoundRoute name="not-found" handler={require('./components/NotFound')}/>
+      <NotFoundRoute name='not-found' handler={require('./components/NotFound')}/>
     </Route>
   ];
 };
