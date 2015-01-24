4a5bb3c9fcc7e7d30cd3de0e5ba0876dc5777d7b
Matthew Smith <mtscout6@gmail.com>
2015-01-24 16:52:08 -0700

    Fix home page post description link style

--- lib/components/PostEntryDescription.jsx
+++ lib/components/PostEntryDescription.jsx
@@ -12,24 +12,26 @@ var PostEntryDescription = React.createClass({
 
   render: function() {
     return (
-      <Link to='post' params={{post: this.props.urlSlug}}>
-        <Row className='blog-post-description'>
-          <Col md={2} sm={3} className='text-center'>
-            <img className='blog-post-icon' src={images['./javascript.png']} alt='' />
-          </Col>
-          <Col md={10} sm={9}>
-            <span className='h4'>
-              <span className='blog-post-description-title'>
-                {this.props.title}
+      <div className='blog-post-description'>
+        <Link to='post' params={{post: this.props.urlSlug}}>
+          <Row>
+            <Col md={2} sm={3} className='text-center'>
+              <img className='blog-post-icon' src={images['./javascript.png']} alt='' />
+            </Col>
+            <Col md={10} sm={9}>
+              <span className='h4'>
+                <span className='blog-post-description-title'>
+                  {this.props.title}
+                </span>
+                <small><em>{this.props.date.format("MMMM D, YYYY")}</em></small>
               </span>
-              <small><em>{this.props.date.format("MMMM D, YYYY")}</em></small>
-            </span>
-            <br/>
-            <span>Sit in soluta dolorem laudantium unde nulla rem soluta id accusantium eius id excepturi. Sunt eius optio esse architecto nisi reiciendis! Sunt nulla blanditiis atque temporibus nostrum. Voluptas asperiores rerum.</span>
-          </Col>
-        </Row>
+              <br/>
+              <span>Sit in soluta dolorem laudantium unde nulla rem soluta id accusantium eius id excepturi. Sunt eius optio esse architecto nisi reiciendis! Sunt nulla blanditiis atque temporibus nostrum. Voluptas asperiores rerum.</span>
+            </Col>
+          </Row>
+        </Link>
         { this.props.divider ? <hr /> : null }
-      </Link>
+      </div>
     );
   }
 });
